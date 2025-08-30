1e0bba6a1db7380775b30107aba4adbe8048d7e6
alammehtab <mehtaba728@gmail.com>
2025-08-30 10:38:31 +0500

    add some basic stuff

---
+++ main.py
@@ -0,0 +1,10 @@
+# helllooooo world in python
+print("Hello, World!")
+
+# 1. Print your name
+print("Ali")
+# 2. Print your favorite quote
+print("Talk is cheap. Show me the code.- Linus Torvalds")
+# 3. Print learning message
+print("I am learning Python with Alkoders!")
+
